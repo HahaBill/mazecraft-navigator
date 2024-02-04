0843f758fb3dc661452521e49ff31b7888de66f9
Bill Nguyen Ton Hoang <bill.nguyentonhoang@live.com>
2024-02-04 18:24:52 +0000

    adding switch statement to handle particular tools and algorithms

--- scripts/generate_layout.js
+++ scripts/generate_layout.js
@@ -6,9 +6,19 @@ const sidenavElement = document.querySelector('nav');
 const tools = ['map', 'start', 'end', 'checkpoint', 'wall'];
 const algorithms = ['dfs', 'bfs', 'dijkstra', 'a_star', 'mst'];
 
+let selectedTool = '';
+let selectedAlgorithm = '';
+
+////// Generating tools and algorithms //////
+
 function createHeaderTool(tool) {
     let toolElement = document.createElement('button');
     toolElement.textContent = `${tool}`;
+
+    toolElement.addEventListener('click', () => {
+        selectedTool = tool;
+        handleTool(selectedTool);
+    })
 
     toolElement.style.backgroundColor = "lightblue";
     toolElement.style.width = "80px";
@@ -22,12 +32,61 @@ function createAlgorithmsChoice(algorithm) {
     let algorithmElement = document.createElement('button');
     algorithmElement.textContent = `${algorithm}`;
 
+    algorithmElement.addEventListener('click', () => {
+        selectedAlgorithm = algorithm;
+        handleAlgorithm(selectedAlgorithm);
+    });
+
     algorithmElement.style.backgroundColor = "lightblue";
     algorithmElement.style.width = "80px";
     algorithmElement.style.height = "80px";
     algorithmElement.style.borderRadius = "8px";
 
     return algorithmElement;
+}
+
+function handleTool(tool) {
+    switch(tool) {
+        case 'map':
+            console.log(`Selected tool: ${tool}.`)
+            break
+        case 'start':
+            console.log(`Selected tool: ${tool}.`)
+            break;
+        case 'end':
+            console.log(`Selected tool: ${tool}.`)
+            break;
+        case 'checkpoint':
+            console.log(`Selected tool: ${tool}.`)
+            break;
+        case 'wall':
+            console.log(`Selected tool: ${tool}.`)
+            break
+        default:
+            console.log('Error: Illegal tool')
+    }
+}
+
+function handleAlgorithm(algorithm) {
+    switch(algorithm) {
+        case 'dfs':
+            console.log(`Selected algorithm: ${algorithm}.`);
+            break;
+        case 'bfs':
+            console.log(`Selected algorithm: ${algorithm}.`);
+            break;
+        case 'dijkstra':
+            console.log(`Selected algorithm: ${algorithm}.`);
+            break;
+        case 'a_star':
+            console.log(`Selected algorithm: ${algorithm}.`);
+            break;
+        case 'mst':
+            console.log(`Selected algorithm: ${algorithm}.`);
+            break;
+        default:
+            console.log('Error: Illegal algorithm');
+    }
 }
 
 (() => {
